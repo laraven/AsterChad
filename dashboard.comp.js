46fd373c8b27a3b79e053271ce12bcdf23031d96
SaliqAhmad <imsalik480@gmail.com>
2023-03-31 17:20:53 +0530

    dashboard done

---
+++ dashboard.comp.js
@@ -0,0 +1,32 @@
+//write style tag for below html
+document.getElementById("import-dashboard").innerHTML = `
+<div>
+        <h1>Dashboard</h1>
+        <div class="dashboard">
+            <div class="dashboard-item">
+                <h2>Item 1</h2>
+                <p>Item 1 description</p>
+            </div>
+            <div class="dashboard-item">
+                <h2>Item 2</h2>
+                <p>Item 2 description</p>
+            </div>
+            <div class="dashboard-item">
+                <h2>Item 3</h2>
+                <p>Item 3 description</p>
+            </div>
+            <div class="dashboard-item">
+                <h2>Item 4</h2>
+                <p>Item 4 description</p>
+            </div>
+            <div class="dashboard-item">
+                <h2>Item 5</h2>
+                <p>Item 5 description</p>
+            </div>
+            <div class="dashboard-item">
+                <h2>Item 6</h2>
+                <p>Item 6 description</p>
+            </div>
+        </div>
+    </div>
+`
